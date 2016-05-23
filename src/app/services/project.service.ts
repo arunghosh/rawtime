7d19dc348c4209709644979c27a7913444b3987a
Arun Ghosh <arunghosh@gmail.com>
2016-05-23 17:54:57 +0530

    trying for jsonp call: not working

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import {URLSearchParams, Jsonp} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ProjectService {
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private jsonp: Jsonp) {}
   
   getProjects(){
-    return Promise.resolve([{
-      name: 'NDash',
-      id: 1
-    }, {
-      name: 'Zine Screen',
-      id: 10, 
-    }])
-    // return this.http
-    //   .get('http://app.rawdatatech.com/api/projects')
-    //   .toPromise()
-    //   .then(response => response);
+    // return Promise.resolve([{
+    //   name: 'NDash',
+    //   id: 1
+    // }, {
+    //   name: 'Zine Screen',
+    //   id: 10, 
+    // }])
+    return this.jsonp
+      .get('http://app.rawdatatech.com/api/projects/')
+      .toPromise()
+      .then(response => response);
   } 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { RawtimeAppComponent, environment } from './app/';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { ProjectService } from './app/services/project.service'
+import { ProjectService } from './app/services/project.service';
+import {JSONP_PROVIDERS, Jsonp} from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(RawtimeAppComponent, [HTTP_PROVIDERS, ProjectService]);
+bootstrap(RawtimeAppComponent, [HTTP_PROVIDERS, ProjectService, JSONP_PROVIDERS]);
 
